e5764c50dacdb82723f9d1021ae698d6e4b28eac
Arsalan <alodhi@fieldtrend.com>
2023-03-18 23:32:57 -0700

    generative ai

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
@@ -10,26 +11,42 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        product = request.form["product"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=product, #prompt=generate_prompt(product),
             temperature=0.6,
+            max_tokens=150,
+            top_p=1,
+            frequency_penalty=1,
+            presence_penalty=1
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        response_text = response.choices[0].text
+        lines = re.split("\d+\. ", response_text)
+        
+        # Split the result string into a list of lines
+        lines = lines.splitlines()
+
+        # Use a list comprehension to strip leading whitespace from each line
+        stripped_lines = [line.lstrip() for line in lines]
+
+        # Join the stripped lines back together into a single string
+        lines = '\n'.join(stripped_lines)
+
+        # lines.remove('')
+        # return redirect(url_for("index", result=lines))
+        return render_template("index.html", result=lines)
+        # return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
+def generate_prompt(product):
+    return """Suggest differentiated topics for a machine learning curriculum to attract students.
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+Product: Machine Learning Engineering
+Ideas: Deep Learning, Reinforcement Learning, Natural Langugage Processing, Computer Vision, Time Series Analysis, Unsupervised Learning, Bayesian Machine Learning, Explainable AI, Transfer Learning, Edge Computing
+Product: {}
+Ideas:""".format(
+        product.capitalize()
     )
